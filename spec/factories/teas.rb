ec041d0a7eec7c6d18172a6432e3019b7ff028b1
Phillip Kamps <phillip.kamps@gmail.com>
2022-07-26 12:48:19 -0600

    Add factories for customers and teas Ensure correct random data is generated when database is seeded

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :tea do
-    name { "MyString" }
-    description { "MyString" }
-    brew_temp_f { "MyString" }
-    brew_time { "MyString" }
+    name { Faker::Tea.variety }
+    description { Faker::Coffee.notes }
+    brew_temp_f { Faker::Number.within(range: 140..212) }
+    brew_time_mins { Faker::Number.within(range: 1..5) }
   end
 end
