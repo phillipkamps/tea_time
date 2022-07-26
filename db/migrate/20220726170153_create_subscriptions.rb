7009a755d49e34a4a19469b19d9048b1a1550b28
Phillip Kamps <phillip.kamps@gmail.com>
2022-07-26 15:20:10 -0600

    Fix data types in CreateSubscriptions migration Price is a float and status is a boolean, this change updates them accordingly

--- db/migrate/20220726170153_create_subscriptions.rb
+++ db/migrate/20220726170153_create_subscriptions.rb
@@ -2,8 +2,8 @@ class CreateSubscriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :subscriptions do |t|
       t.string :title
-      t.string :price
-      t.string :status
+      t.float :price
+      t.boolean :status
       t.string :frequency
       t.references :customer, foreign_key: true
       t.references :tea, foreign_key: true
