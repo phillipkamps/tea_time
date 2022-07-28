57bee066bfd6293ded882fefb1578d5726d99734
Phillip Kamps <phillip.kamps@gmail.com>
2022-07-28 09:13:04 -0600

    Add endpoint + tests to create subscription Use gem for serializer

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  def create
+    @customer = Customer.find(params[:customer_id])
+    @tea = Tea.find(params[:tea_id])
+    @subscription = @customer.subscriptions.new(subscription_params)
+    @subscription.update(title: "#{@customer.first_name}'s #{@tea.name} Subscription",
+                         status: 'Active')
+    @subscription.save
+    render json: SubscriptionSerializer.new(@subscription), status: :created
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:price, :frequency, :tea_id)
+  end
+end
--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -1,9 +1,56 @@
 require 'rails_helper'
 
 describe 'Tea Time API' do
+  let!(:customer) { create(:customer) }
+  let!(:tea) { create(:tea) }
+
   it 'creates a subscription to a tea for a customer' do
-    post '/api/v1/subscriptions'
+    subscription_params = {
+      title: "#{customer.first_name}'s #{tea.name} Subscription",
+      price: '4.99',
+      status: 'Active',
+      frequency: 'Monthly',
+      tea_id: tea.id,
+      customer_id: customer.id
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+    post "/api/v1/customers/#{customer.id}/subscriptions/#{tea.id}",
+         headers: headers,
+         params: JSON.generate(subscription: subscription_params)
+
+    subscription_json = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(response).to be_successful
+    expect(subscription_json[:type]).to eq 'subscription'
+    expect(subscription_json[:attributes].count).to eq 6
+    expect(subscription_json[:attributes][:customer_id]).to eq customer.id
+    expect(subscription_json[:attributes][:tea_id]).to eq tea.id
+  end
+
+  it 'updates status of subscription' do
+    subscription_params = {
+      title: "#{customer.first_name}'s #{tea.name} Subscription",
+      price: '4.99',
+      status: 'Active',
+      frequency: 'Monthly',
+      tea_id: tea.id,
+      customer_id: customer.id
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+    post "/api/v1/customers/#{customer.id}/subscriptions/#{tea.id}",
+         headers: headers,
+         params: JSON.generate(subscription: subscription_params)
+
+    expect(response).to be_successful
+    previous_status = Subscription.last.status
+
+    update_status_params = {
+      status: 'Cancelled'
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+
+    patch "/api/v1/customers/#{customer.id}/subscriptions/#{Subscription.last.id}", headers: headers,
+                                                                                    params: JSON.generate(subscription: update_status_params)
+    binding.pry
   end
 end
