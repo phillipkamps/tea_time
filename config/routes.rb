3de5a8d032ce661e7f3499f124fee61dc985a3d1
Phillip Kamps <phillip.kamps@gmail.com>
2022-07-28 09:13:51 -0600

    Add routes to add and edit subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post '/api/v1/customers/:customer_id/subscriptions/:tea_id', to: 'api/v1/subscriptions#create'
+
+  patch '/api/v1/customers/:customer_id/subscriptions/:subscription_id', to: 'api/v1/subscriptions#update'
 end
